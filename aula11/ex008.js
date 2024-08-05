910e0497ef97a3f31c359895c8339f1482929773
Roger Antonio <roger.a.sou@gmail.com>
2024-08-05 09:20:47 -0300

    atualizaçao

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 85
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){// Condição Simples!
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO')
+}
+
+console.log('Dirija sempre Usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`vivendo em ${pais}`)
+if (pais != 'Brasil'){
+    console.log('voce é estrangeiro')
+}else {
+    console.log('voce é brasileiro')
+}
